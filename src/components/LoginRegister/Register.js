06d928d019b577a9dbdaf45f5290f5f39285bdbf
Kevin Do <91227948+KevintrDo@users.noreply.github.com>
2024-02-22 02:16:29 -0500

    REgister DONE

--- src/components/LoginRegister/Register.js
+++ src/components/LoginRegister/Register.js
@@ -8,90 +8,84 @@ const Register = () => {
     <div className="containerForm">
       <form className="bodyRegisterForm">
       <div>
-      <div className="columnForm">
         <h2>User Information</h2>
         <div className="form-group">
           <label>Username: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>Email: </label>
-          <input type="email" />
+          <input class="forms-inputRegister" type="email" />
         </div>
         <div className="form-group">
           <label>Password: </label>
-          <input type="password" />
+          <input class="forms-inputRegister" type="password" />
         </div>
         <div className="form-group">
           <label>Confirm Password: </label>
-          <input type="password" />
+          <input class="forms-inputRegister" type="password" />
         </div>
         <div className="form-group">
           <label>Phone Number: </label>
-          <input type="tel" />
+          <input class="forms-inputRegister" type="tel" />
         </div>
-      </div>
-
-    <div className="columnForm">
       <div className="form-group">      
         <h2>Payment Information</h2>      
           <label>Card Type: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>Card Number: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>Expiration Date: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
-    </div>
-
+        <div className="form-group">        
+          <button class="registerButtonRegister">Register!</button>
+        </div>        
     </div>
     
+    
     <div>
-      <div className="columnForm">
         <h2>Home Address</h2>
         <div className="form-group">
           <label>Street: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>City: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>State: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>Zip Code: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
-      </div>
 
-      <div className="columnForm">
         <h2>Billing Address</h2>
         <div className="form-group">
           <label>Street: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>City: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>State: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
         <div className="form-group">
           <label>Zip Code: </label>
-          <input type="text" />
+          <input class="forms-inputRegister" type="text" />
         </div>
-      </div>
-      <button>Register!</button>
-      
+        <div className="fillInSpaceRegister"></div>
+        
       </div>
       </form>
       </div>
