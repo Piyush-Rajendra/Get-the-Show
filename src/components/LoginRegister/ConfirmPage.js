738d9188fae51e62f67651c68f563a99cf20019c
Kevin Do <91227948+KevintrDo@users.noreply.github.com>
2024-02-23 21:39:19 -0500

    Confirmation page

--- src/components/LoginRegister/ConfirmPage.js
+++ src/components/LoginRegister/ConfirmPage.js
@@ -1,38 +1,24 @@
 import React from "react";
-import './../css/LoginRegister/Register.css';
+import './../css/LoginRegister/ConfirmPage.css';
 import {Link, useState } from 'react';
+import checkMark from './checkMark.png'
+
   
 
 //import UserContext from "./context/UserContext";
 
 const ConfirmPage = () => {
 
-   const [displayText, setDisplayText] = useState('Please enter your email and we will send a password change.');
-
-    const handleClick = () => {
-        setDisplayText('Thank you! The email should be send to your inbox!');
-    };
 
     return (
     <div class = "background">
           <hr></hr>
-          <h2 class="register">Forgot Password</h2>
+          <h2 class="register">Thank You For Signing Up!</h2>
         <div className="center">
-        <div class="formcontainer">
-          <form className ="forms">
-          <h3>{displayText}</h3>
-            <label class="forms-label">
-              Email
-              <input class="forms-input"
-                type="email"
-                name="email"
-                required
-              />
-            </label>
-            <br />
-            <button onClick={handleClick} className="register-button">Confirm</button>
-          </form>
-      </div>
+        <div className="formsConfirmationPage">
+            <img className="imageConfirmationPage" src={checkMark}/>
+            <h3 className="h3ConfirmationPage">A confirmation has been sent to your email!</h3>         
+        </div>
       </div>
     </div>
     )
