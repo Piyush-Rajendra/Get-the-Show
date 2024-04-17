94eb303a5fee13c2a0834048b178783334c336ef
Kevin Do <91227948+KevintrDo@users.noreply.github.com>
2024-04-16 21:03:55 -0400

    Save changes

--- src/components/LoginRegister/RegisterContinued.js
+++ src/components/LoginRegister/RegisterContinued.js
@@ -83,6 +83,21 @@ const handleSubmit = async (e) => {
   }
 };
 
+function formatDate(dateString) {
+    if (!dateString) return '';
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) {
+      month = 0${month};
+    }
+    let day = date.getDate();
+    if (day < 10) {
+      day = 0${day};
+    }
+    return ${year}-${month}-${day};
+  }
+
  return (
   <div className="centerFormRegister">  
     <div className="containerForm">
